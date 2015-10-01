c9af73918ff6c63e482bddd9eff818e5336aa4a6
kPaulNg <kpaul.ng@gmail.com>
2015-10-01 17:30:33 +0800

    update

--- www/js/user.js
+++ www/js/user.js
@@ -4,13 +4,13 @@ angular.module('starter.user', [])
 .controller("user", function($scope, $http) {
 
     //GET PRODUCT TYPE
-    $http.get('http://staging.wine-enterprise.com:8011/apis/producttype')
-      .then(function(response) {
-        $scope.productTypeList = response.data;
-        $scope.img = "http://placehold.it/50x50";
-      }, function(err){
-          console.error('ERR', err);
-      })
+    $http.post('http://staging.wine-enterprise.com:8011/apis/user/registration',
+    	{username : '123'})
+    	.success(function(response){
+    		$scope.response = response.data;
+    		console.log(response.data);
+    	})
+
 
 
 //LOGIN
