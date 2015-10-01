0f8eb293ef0a5bb03ba4e4820787ebbdead38f83
Hunge <hunge@fteg.com.my>
2015-10-01 11:53:00 +0800

    add in hardcode data

--- www/js/productdetails.js
+++ www/js/productdetails.js
@@ -20,5 +20,25 @@ angular.module('starter.productdetails', [])
   //         console.error('ERR', err);
   //     })
 
-	$scope.quote = [{'product':'whisky'}];
+	$scope.productDetails = [];
+
+	$scope.productData = [
+		{"id": "18",
+		 "sku_code": "BW002",
+		 "name": "JOHNNIE WALKER BLACK LABEL 12 Y",
+		 "id_productType": "4",
+		 "status": "Active",
+		 "created_on": "2015-06-24 02:18:13",
+		 "type": "BLENDED WHISKEY",
+		 "price": "168.00",
+		 "year": "12",
+		 "id_countryMaster": "2",
+		 "origin": "scotland",
+		 "note": "It comprises some 40 different malts and grains, R ...",
+		 "alcohol": "40.00",
+		 "size": "70CL",
+		 "photo": "12345.png",
+		 "balance": "0.00"
+		}];
+	
 });
