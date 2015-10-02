67e57fbaa172f638ea8c5250a5ba876e5b1fc4b7
kPaulNg <kpaul.ng@gmail.com>
2015-10-02 19:14:41 +0800

    yes

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -11,7 +11,9 @@ angular.module('starter.controllers', [])
   //});
 
   // Form data for the login modal
-  $scope.loginData = {};
+  $scope.loginData = {
+    'src': 'img/ionic.png'
+  };
 
   // Create the login modal that we will use later
   $ionicModal.fromTemplateUrl('templates/login.html', {
