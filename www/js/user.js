5d0cb4b16c2f1eaeaf5fd7d7c5c097366eceb908
hh0210 <fung@fteg.com.my>
2015-10-02 23:28:05 +0800

    post testing done

--- www/js/user.js
+++ www/js/user.js
@@ -4,12 +4,29 @@ angular.module('starter.user', [])
 .controller("user", function($scope, $http) {
 
     //GET PRODUCT TYPE
- 	$http.post('http://staging.wine-enterprise.com:8011/apis/user/registration')
-      .then(function(response) {
+	console.error('INFO', 'A');
+	$http({
+	    method: 'POST',
+	    url: 'http://staging.wine-enterprise.com:8011/apis/user/registration',
+	    data: "username=" + 'username3' + Math.random() + '&password=' + 'passwod2',
+	    // data: {"username=":'username1','password=':'passwod1'},
+	    headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+	    // headers: {'Content-Type': 'application/json'}
+	    responseType :'json',
+	}).then(function successCallback(response) {
+		console.log('INFO', response);
+		console.log('INFO', response.data.status);
         $scope.test = response.data;
-      }, function(err){
-          console.error('ERR', err);
-      })
+	}, function errorCallback(response) {
+		console.log('ERROR', response);
+	});
+
+ 	// $http.post('http://staging.wine-enterprise.com:8011/apis/user/registration')
+  //     .then(function(response) {
+  //       $scope.test = response.data;
+  //     }, function(err){
+  //         console.error('ERR', err);
+  //     })
 
 //LOGIN
 	$scope.loginData = {
