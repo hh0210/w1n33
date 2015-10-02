32247452d9c3bb1df57a7df0ca3b70d4b80745d6
kPaulNg <kpaul.ng@gmail.com>
2015-10-02 18:46:29 +0800

    updates

---
+++ www/js/payment.js
@@ -0,0 +1,14 @@
+angular.module('starter.payment', [])
+
+//CATEGORIES
+.controller("payment", function($scope, $http) {
+
+    //GET PRODUCT TYPE
+    $http.get('http://staging.wine-enterprise.com:8011/apis/producttype')
+      .then(function(response) {
+        $scope.productTypeList = response.data;
+        $scope.img = "http://placehold.it/50x50";
+      }, function(err){
+          console.error('ERR', err);
+      })
+});
