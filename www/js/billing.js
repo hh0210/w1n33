4f76a26b666e3108250624fa0bb941da2706f01b
kPaulNg <kpaul.ng@gmail.com>
2015-10-07 15:49:53 +0800

    billing update

--- www/js/billing.js
+++ www/js/billing.js
@@ -3,12 +3,21 @@ angular.module('starter.billing', [])
 //BILLING
 .controller("billing", function($scope, $http, $state) {
 
+	//cart id
+	if(localStorage.getItem('cart_id') != null){
+		var cart_id = JSON.parse(localStorage.getItem('cart_id'));
+		console.log('current cart_id',cart_id);
+	}else{
+		var cart_id = '';
+		console.log('current cart_id',cart_id);
+	};
+
     //Send Billing form data
 	$scope.billingForm = function(billing){
 		$http({
 		    method: 'POST',
 		    url: 'http://staging.wine-enterprise.com:8011/apis/sales/order/person',
-		    data: 'salesorder_id='+ '123' + '&given_name=' + billing.firstName + '&family_name=' + billing.lastName
+		    data: 'cart_id='+ cart_id + '&given_name=' + billing.firstName + '&family_name=' + billing.lastName
 		          + '&email=' + billing.email + '&phone=' + billing.phone +
 		           '&address=' + billing.address,
 		    headers: {'Content-Type': 'application/x-www-form-urlencoded'},
@@ -16,16 +25,14 @@ angular.module('starter.billing', [])
 		}).then(function successCallback(response) {
 			console.log('INFO', response);
 			console.log('INFO', response.data.status);
-			console.log("###################################");
-	        $scope.test = response.data;
-
+	        console.log('INFO', response.data.billingInfo);
 			$scope.toShipping = function() {
 				$state.go('app.shipping');
 			}
 	        
 		}, function errorCallback(response) {
 			console.log('ERROR', response);
-			console.log('############# Error');
+
 		});
 	}
 });
