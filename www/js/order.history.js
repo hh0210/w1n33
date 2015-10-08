6477cc66258ab439f2a92238211095411619ac27
Hunge <hunge@fteg.com.my>
2015-10-08 18:55:10 +0800

    updated some changes

--- www/js/order.history.js
+++ www/js/order.history.js
@@ -3,7 +3,7 @@ angular.module('starter.orderhistory', [])
 .controller("orderhistory", function($scope, $http) {
 
 	// Only registered user can view order history.
-	if(localStorage.getItem('loginInfo') != null){
+	if (localStorage.getItem('loginInfo') != null) {
 		$scope.user = JSON.parse(localStorage.getItem('loginInfo'));
 		var user_id = $scope.user.id;
 		console.log('localstorage USER ID',user_id);
@@ -18,5 +18,5 @@ angular.module('starter.orderhistory', [])
 		  }, function(err){
 		      console.error('ERR', err);
 		  })
-	}
+	} 
 });
