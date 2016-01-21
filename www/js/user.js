d27ae1f81e31dac969ff81c7b9c274b334a03166
kPaulNg <kpaul.ng@gmail.com>
2016-01-21 14:14:22 +0800

    listing and profile updated

--- www/js/user.js
+++ www/js/user.js
@@ -77,4 +77,59 @@ angular.module('starter.user', ['ngMessages'])
     // }, 3000);
    };
 
+  //profile
+
+  // Only registered user can view order history.
+  if (localStorage.getItem('loginInfo') != null) {
+    $scope.user = JSON.parse(localStorage.getItem('loginInfo'));
+    var user_id = $scope.user.id;
+    console.log('localstorage USER ID',user_id);
+  }
+
+  $http.get('http://staging.wine-enterprise.com:8011/apis/user/profile?user_id='+user_id)
+    .then(function(response) {
+        $scope.userdata = response.data;
+        console.log($scope.userdata);
+      }, function(err){
+          console.error('ERR', err);
+  })
+
+  $scope.profileform = function(userdata){
+    $http({
+        method: 'POST',
+        url: 'http://staging.wine-enterprise.com:8011/apis/user/profile',
+        data: 'user_id=' + userdata.id_UserMaster + '&given_name=' + userdata.given_name + '&family_name=' + userdata.family_name +
+              '&email=' + userdata.email + '&phone=' + userdata.phone +
+              '&address1=' + userdata.address1 + '&address2=' + userdata.address2 +
+              '&postcode=' + userdata.postcode + '&city=' + userdata.city,
+        // data: {"username=":'username1','password=':'passwod1'},
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+        // headers: {'Content-Type': 'application/json'}
+        responseType :'json',
+    }).then(function successCallback(response) {
+      console.log('INFO', response);
+      // console.log('INFO', response.data.status);
+      
+       // Popup message for logout sucessfully.
+       var alertPopup = $ionicPopup.alert({
+          title: 'Message',
+          template: 'You have successfully save!'
+       });
+       alertPopup.then(function(res) {
+           
+       });
+
+       // Close the popup message and modal and go to home page.
+       $timeout(function(){
+          alertPopup.close();
+       }, 1000);
+
+       $scope.closeLogin();
+       $state.go('app.user-profile');
+
+    }, function errorCallback(response) {
+      console.log('ERROR', response);
+    });
+  };
+
 });
