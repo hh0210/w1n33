13a3c4ef9d95d3942eabc224087eeb287de89e6c
kPaulNg <kpaul.ng@gmail.com>
2016-01-22 16:36:47 +0800

    half sales flow fixed

--- www/js/shipping.js
+++ www/js/shipping.js
@@ -1,89 +1,94 @@
 angular.module('starter.shipping', [])
 
-//SHIPPING
 .controller("shipping", function($scope, $http, $stateParams, $state) {
 
-	//cart id
-	if(localStorage.getItem('cart_id') != null){
-		var cart_id = JSON.parse(localStorage.getItem('cart_id'));
-		console.log('current cart_id',cart_id);
-	}else{
-		var cart_id = '';
-		console.log('current cart_id',cart_id);
-	};
+	// cart id
+	var cart_id = (localStorage.getItem('cart_id'))?JSON.parse(localStorage.getItem('cart_id')):'';
+	console.log('cart_id',cart_id);
 
-	//get shipment method
+	//shipment type list
     $http.get('http://staging.wine-enterprise.com:8011/apis/sales/order/shipment/type')
       .then(function(response) {
-        $scope.shipmentList = response.data;
-        console.log(response);
-        console.log('type',$scope.shipmentList);
-      }, function(err){
-          console.error('ERR', err);
-      });
+        $scope.ShipmentList = response.data;
+        console.log('shipment list',$scope.ShipmentList);
+	}, function(err){
+	  console.error('error', err);
+	});
 
-    //get shipment method
+    //location list
     $http.get('http://staging.wine-enterprise.com:8011/apis/sales/order/selfpickup/location')
       .then(function(response) {
         $scope.locationList = response.data;
-        console.log(response);
-        console.log('location',$scope.locationList);
-      }, function(err){
-          console.error('ERR', err);
-      });
+        console.log('location list',$scope.locationList);
+  	}, function(err){
+          console.error('error', err);
+	});
 
+  	//get shipment info
+    $http.get('http://staging.wine-enterprise.com:8011/apis/sales/order/shipment?cart_id='+cart_id)
+      .then(function(response) {
+        $scope.shipment = response.data;
+        console.log('shipment info',$scope.shipment);
+  	}, function(err){
+          console.error('error', err);
+	});
+
+    //get selfpickup info
+    $http.get('http://staging.wine-enterprise.com:8011/apis/sales/order/selfpickup?cart_id='+cart_id)
+      .then(function(response) {
+        $scope.selfpickupinfo = response.data;
+        console.log('location info', $scope.selfpickupinfo);
+  	}, function(err){
+        console.error('error', err);
+	});
 
     //selection
 	$scope.showSelectValue = function(mySelect) {
-    	console.log(mySelect);
     	$scope.item_id = mySelect;
+    	console.log('shipment_id', $scope.item_id);
 	}
 
-    //Send shipping form data
-	$scope.shipment = function(shipmentInfo){
+    //post shipping data
+	$scope.ShipmentForm = function(shipment){
 		$http({
 		    method: 'POST',
 		    url: 'http://staging.wine-enterprise.com:8011/apis/sales/order/shipment',
-		    data: 'cart_id='+ cart_id + '&given_name=' + shipmentInfo.given_name + '&family_name=' + shipmentInfo.family_name +
-		          '&phone=' + shipmentInfo.phone + 
-		          '&address=' + shipmentInfo.address1 + '\n' + shipmentInfo.address2 + '\n' + shipmentInfo.city + '\n' + shipmentInfo.postcode +
-		          '&id_SalesOrderShipmentType=' + $scope.item_id,
+		    data: 'cart_id='+ cart_id + '&given_name=' + shipment.given_name + '&family_name=' + shipment.family_name +
+		          '&phone=' + shipment.phone + 
+		          '&address1=' + shipment.address1 + '&address2=' + shipment.address2 +
+              	  '&postcode=' + shipment.postcode + '&city=' + shipment.city +
+              	  '&shipment_id=' + $scope.item_id,
 		    headers: {'Content-Type': 'application/x-www-form-urlencoded'},
 		    responseType :'json',
 		}).then(function successCallback(response) {
-			console.log('INFO', response);
-			// console.log('STATUS', response.data.status);
-
+			console.log('success: ', response);
 			$state.go('app.ordersummary');
-	        
 		}, function errorCallback(response) {
-			console.log('ERROR', response);
+			console.log('error', response);
 		});
 	}
 
-	//Send selfpickup form data
+	//post selfpickup data
 	$scope.selfpickup = function(locationInfo){
 		$http({
 		    method: 'POST',
 		    url: 'http://staging.wine-enterprise.com:8011/apis/sales/order/selfpickup',
-		    data: 'cart_id='+ cart_id + '&id_SelfPickUpLocation=' + $scope.item_id,
+		    data: 'cart_id='+ cart_id + '&selfpickup_id=' + $scope.item_id,
 		    headers: {'Content-Type': 'application/x-www-form-urlencoded'},
 		    responseType :'json',
 		}).then(function successCallback(response) {
-			console.log('INFO', response);
-			console.log('STATUS', response.data.status);
-
+			console.log('success: ', response);
 			$state.go('app.ordersummary');
 	        
 		}, function errorCallback(response) {
-			console.log('ERROR', response);
+			console.log('error', response);
 		});
 	}
 
-	// Retrieve billing user data to display at shipping page
+	// retrieve billing user data
 	$scope.identifyUser = function(checkboxData) {
 
-		// If user selected ship to himself, get all billing data.
+		// select to get billing data
 		if (checkboxData === true) {
 		$http({
 			 url: 'http://staging.wine-enterprise.com:8011/apis/sales/order/person',
@@ -91,28 +96,12 @@ angular.module('starter.shipping', [])
 			 params: {cart_id: cart_id}
 		   })
 	      .then(function(response) {
-	        $scope.shipmentInfo = response.data;
-
-	        // Split address by newline
-	      	$scope.addressInfo = $scope.shipmentInfo.address.split("\n");
-	      	$scope.shipmentInfo.address1 = $scope.addressInfo[0];
-	      	$scope.shipmentInfo.address2 = $scope.addressInfo[1];
-	      	$scope.shipmentInfo.city = $scope.addressInfo[2];
-	      	$scope.shipmentInfo.postcode = $scope.addressInfo[3];
+	        $scope.shipment = response.data;
+	        console.log('shipment info', $scope.shipment);
 	     
 	      }, function(err){
-	          console.error('ERR', err);
+	          console.error('error', err);
 	      });
-		} else{
-			
-			// Set all form fields to null.
-			$scope.shipmentInfo.given_name = null;
-			$scope.shipmentInfo.family_name = null;
-			$scope.shipmentInfo.phone = null;
-			$scope.shipmentInfo.address1 = null;
-			$scope.shipmentInfo.address2 = null;
-			$scope.shipmentInfo.city = null;
-			$scope.shipmentInfo.postcode = null;
 		}
 	} 
 });
